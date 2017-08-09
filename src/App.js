4bd8e460611849a78e3454bb1599439555d6e74b
Dr.Code <stweb.it@gmail.com>
2017-08-09 20:53:59 +1000

    поделючилсся к firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import  firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillAmount() {
+    const config = {
+  apiKey: 'AIzaSyCFn5yyIhIdnflc5x2-Z3OOVqJgeptRRco',
+  authDomain: 'manager-dae7a.firebaseapp.com',
+  databaseURL: 'https://manager-dae7a.firebaseio.com',
+  projectId: 'manager-dae7a',
+  storageBucket: 'manager-dae7a.appspot.com',
+  messagingSenderId: '160912747035'
+
+
+firebase.initializeApp(config);
+
+}
   render() {
     return(
       <Provider store={createStore(reducers)}>
