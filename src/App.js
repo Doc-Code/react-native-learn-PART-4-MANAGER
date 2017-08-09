b053c09fee1b4079f2f68636e74ca5062c70be68
Dr.Code <stweb.it@gmail.com>
2017-08-09 20:12:38 +1000

    Создал app исправил индекс

--- src/App.js
+++ src/App.js
@@ -2,3 +2,19 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux0;'
+
+class App extends Component {
+  render() {
+    return(
+      <Provider store={createStore()}>
+        <View>
+          <Text>
+            ПРивет мир!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
