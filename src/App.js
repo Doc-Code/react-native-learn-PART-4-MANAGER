46ea0fc9f36080ec6ba4e9db6eae11438bcd904c
Dr.Code <stweb.it@gmail.com>
2017-08-09 20:35:29 +1000

    Сук. когда исправлял ощибку снова допустил ошибку
    Снова исправил

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux0';
+import { createStore } from 'redux';
 
 class App extends Component {
   render() {
