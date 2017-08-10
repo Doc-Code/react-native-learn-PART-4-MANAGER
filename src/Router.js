4d3df20e158d3630a93f35c914822440effe0007
Dr.Code <stweb.it@gmail.com>
2017-08-11 01:55:46 +1000

    новая сцена редактировать + меняются данные \\пока только в консоле

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
   return (
@@ -28,11 +29,12 @@ const RouterComponent = () => {
         <Scene
           key="employeeCreate"
           component={EmployeeCreate}
-          title="новый сотрудник?"
+          title="Добавить нового"
         />
       </Scene>
+      <Scene key="employeeEdit" component={EmployeeEdit} title="Редактировать" />
 
-      </Router>
+    </Router>
 
   );
 };
