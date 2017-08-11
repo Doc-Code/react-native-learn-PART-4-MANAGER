4d95ef774913c7bd763b66051a5c231a0815e310
Dr.Code <stweb.it@gmail.com>
2017-08-11 17:03:44 +1000

    Создал компонент СONFIRM

--- src/components/common/Cornfirm.js
+++ src/components/common/Cornfirm.js
@@ -2,8 +2,27 @@ import React from 'react';
 import { Text, View, Modal } from 'react-native';
 import { CardSection, Button } from './';
 
-const Confirm = () => {
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="slide"
+      onRequestClose={() => {}}
+    >
+      <View>
+        <CardSection>
+          <Text>{children}</Text>
+        </CardSection>
 
+        <CardSection>
+          <Button onPress={onAccept}>Да</Button>
+          <Button onPress={onDecline}>Нет</Button>
+        </CardSection>
+
+      </View>
+    </Modal>
+);
 };
 
 export { Confirm };
